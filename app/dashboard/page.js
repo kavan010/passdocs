7a289744aecf14ddbb23ca96773142fa6921b8a8
kavan010 <kavangill05@gmail.com>
2025-10-01 11:21:34 -0400

    amplify yml

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -9,7 +9,7 @@ export default function DashboardPage() {
     }
     if (!session) {
         if (typeof window !== 'undefined') {
-            window.location.href = "/signin";
+            window.location.href = "/api/auth/signin";
         }
         return null;
     }
@@ -18,6 +18,9 @@ export default function DashboardPage() {
             <Navbar />
             <div className="w-3xl h-auto bg-gray-200 flex flex-col justify-center items-center mt-50">
                 <h1 className="text-2xl font-bold text-center">Welcome to your Dashboard, {session.user.name}</h1>
+                <button 
+                    onClick={() => signOut({ redirect: false })}
+                    className="mt-4 px-2 py-1 bg-blue-500 text-white rounded-2xl hover:bg-blue-400 transition duraction-200">Sign Out</button>
             </div>
         </div>
     );
