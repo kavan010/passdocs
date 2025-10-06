859b06ca03c4de0a8bec553202a8afe1dc4699e1
kavan <kavangill05@gmail.com>
2025-10-05 22:05:40 -0400

    Update route.js

--- app/api/testenv/route.js
+++ app/api/testenv/route.js
@@ -1,7 +1,10 @@
 export async function GET(req) {
   return new Response(JSON.stringify({
+    NEXTAUTH_URL: !!process.env.NEXTAUTH_URL,
+    GOOGLE_CLIENT_ID: !!process.env.GOOGLE_CLIENT_ID,
+    NEXTAUTH_SECRET: !!process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    NEXTAUTH_SECRET: !!process.env.NEXTAUTH_SECRET
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   }));
 }
